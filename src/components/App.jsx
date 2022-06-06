f74ee9e707b52e19170ced979b98d54ab62188ee
Irina Skopetskaya <sairina2704@gmail.com>
2022-06-06 03:17:53 +0300

    contacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,67 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+import ContactItems from './ContactItems';
+import Filter from './Filter';
+import s from './App.module.css';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+  formSubmit = data => {
+    const { contacts } = this.state;
+
+    contacts.find(
+      contact => contact.name.toLowerCase() === data.name.toLowerCase()
+    )
+      ? alert(`${data.name} is already in contact`)
+      : this.setState(prevState => ({
+          contacts: [data, ...prevState.contacts],
+        }));
+  };
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+  filterByLetters = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().startsWith(normalizedFilter)
+    );
+  };
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+  render() {
+    const { filter } = this.state;
+    return (
+      <div className={s.app}>
+        <ContactForm onSubmit={this.formSubmit} />
+        <h3>Contacts</h3>
+        <Filter filter={filter} onChange={this.changeFilter} />
+        <ContactList>
+          <ContactItems
+            contacts={this.filterByLetters()}
+            onDeleteContact={this.deleteContact}
+          />
+        </ContactList>
+      </div>
+    );
+  }
+}
+ContactForm.propTypes = {
+  name: PropTypes.string,
+  number: PropTypes.number,
 };
+
+export default App;
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types';
+
+function ContactList({ children }) {
+    return <ul>{children}</ul>;
+  }
+  
+  ContactList.propTypes = {
+    children: PropTypes.node.isRequired,
+  };
+  
+  export default ContactList;
